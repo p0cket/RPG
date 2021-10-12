017f6238ec59a56366a9046d8dfd9a4b1cdc0c27
Pocket <p0ck31@gmail.com>
2021-10-12 00:37:41 -0400

    Feat: Added ShopKeeper and some of his code

---
+++ Assets/Scripts/ShopKeeper.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopKeeper : MonoBehaviour
+{
+    private bool canOpen;
+    public string[] ItemsForSale = new string[50];
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Fire1");
+        }
+        if(canOpen && Input.GetButtonDown("Fire1") && PlayerController.instance.canMove && !Shop.instance.shopMenu.activeInHierarchy)
+        {
+            Shop.instance.itemsForSale = ItemsForSale;
+
+            Shop.instance.OpenShop();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            canOpen = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            canOpen = false;
+        }
+    }
+}
