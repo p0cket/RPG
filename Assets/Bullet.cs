e8a37f3c91ceec2fac40a5ae6cf27a4089d4f3cc
Pocket <p0ck31@gmail.com>
2021-10-19 00:13:25 -0400

    Added Shooting to Other Characters and auto-moving with Delay

---
+++ Assets/Bullet.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    public int soundEffect;
+    public int bulletDamage;
+
+    public float timeUntilDestroyed = 2f;
+
+    // void Awake(){
+    //   Destroy(gameObject, timeUntilDestroyed);
+    // }
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+        AudioManager.instance.PlaySFX(soundEffect);
+        
+        // wait and then destroy 
+        // yield WaitForSeconds(timeUntilDestroyed);
+        // Destroy(gameObject);
+
+    }
+
+    void OnTriggerEnter2D (Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(bulletDamage);
+        }
+        // Debug.Log(hitInfo.name);
+        Destroy(gameObject);
+    }
+}
