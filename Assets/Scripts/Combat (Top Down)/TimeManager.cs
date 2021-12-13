caa1ad75adca9bc9998465ebd9a2c2ac270ce60b
Pocket <p0ck31@gmail.com>
2021-12-12 22:41:26 -0500

    chore: before progressbar refactor

--- Assets/Scripts/Combat (Top Down)/TimeManager.cs
+++ Assets/Scripts/Combat (Top Down)/TimeManager.cs
@@ -8,35 +8,17 @@ public class TimeManager : MonoBehaviour
     public float slowdownFactor = 0.05f;
     public float slowdownLength = 2f;
 
-    public float freezeDuration = 4.00f;
 
+    // So, weird bug here, but this second script being attached allows slowdown to happen super quickly
     void Update()
     {
         Time.timeScale += (1f / slowdownLength) * Time.unscaledDeltaTime;
         Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
     }
 
-    public void DoSlowmotion ()
-    {
-        Time.timeScale = slowdownFactor;
-        Time.fixedDeltaTime = Time.timeScale * 0.02f;
-    }
-
-    public void FreezeTime(float duration)
-    {
-        // Uncomment this and add it as a param of Wait() to add the last timespeed
-        // float curTimespeed = Time.timeScale;
-
-        // 0 will stop time
-        Time.timeScale = 0.0f;
-        StartCoroutine(Wait(duration));
-    }
-
-    IEnumerator Wait(float duration)
-    {
-        yield return new WaitForSecondsRealtime(duration);
-        // if you want to set it back to current time speed use below. Otherwise, set back to 1
-        // Time.timeScale = curTimespeed;
-        Time.timeScale = 1.0f;
-    }
+    // public void DoSlowmotion ()
+    // {
+    //     Time.timeScale = slowdownFactor;
+    //     Time.fixedDeltaTime = Time.timeScale * 0.02f;
+    // }
 }
